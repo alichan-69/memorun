06f2dded7492fca96a8ada9094f7601ad79b79b4
alichan <alice.hitugi@gmail.com>
2021-12-31 23:23:53 +0900

    composer.lock追加

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '188f02649b87130dce4798c78a51b9209d89af1c',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '188f02649b87130dce4798c78a51b9209d89af1c',
+            'dev_requirement' => false,
+        ),
+    ),
+);
